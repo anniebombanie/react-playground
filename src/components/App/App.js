30bf29809c5c8ee930e91905c5984f2e671ede9b
anniebombanie <anniebombanie@gmail.com>
2021-09-14 13:46:19 -0400

    working on routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import Groceries from "../Groceries/Groceries";
-import "./App.css";
 // import Product from '../Product/Product--class';
+// import Groceries from "../Groceries/Groceries";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
+import "./App.css";
 
 function App() {
   return (
     // <Product />
-    <Groceries />
+    // <Groceries />
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
