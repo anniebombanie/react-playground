825d1892178db497a5013361d7804a31e7eb0266
anniebombanie <anniebombanie@gmail.com>
2021-09-14 14:27:06 -0400

    login complete w/o storage persistance

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 // import Product from '../Product/Product--class';
 // import Groceries from "../Groceries/Groceries";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Preferences from "../Preferences/Preferences";
+import Login from '../Login/Login';
 import "./App.css";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     // <Product />
     // <Groceries />
