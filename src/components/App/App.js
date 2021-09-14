4044c6715a466c09a3b39ee3cf434969671d4708
anniebombanie <anniebombanie@gmail.com>
2021-09-14 14:39:46 -0400

    session storage token created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,15 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Preferences from "../Preferences/Preferences";
-import Login from '../Login/Login';
+import Login from "../Login/Login";
+import useToken from "./useToken";
 import "./App.css";
 
 function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
-  if(!token) {
-    return <Login setToken={setToken} />
+  if (!token) {
+    return <Login setToken={setToken} />;
   }
 
   return (
